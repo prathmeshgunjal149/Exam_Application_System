67415e4e518fb4b55c5e0d62b28b1e7b0dd8709d
Rutuja <rutujar0407@gmail.com>
2025-06-11 15:30:36 +0530

    changes in login

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 let express=require("express");
 let app=express();
  let conn=require("./config/db.js");
+const bodyParser = require("body-parser");
 
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+let session=require("express-session");
 
-
+app.set("view engine",ejs);
 module.exports=app;
