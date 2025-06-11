bafe810044e0d8b373d1885ecfcca123ad5cd08e
Rutuja <rutujar0407@gmail.com>
2025-06-11 16:42:16 +0530

    chnages done

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ let app = express();
 let conn = require("./config/db.js");
 const homeRoutes = require("./routes/homeRouts");
 app.use("/", homeRoutes);
-app.set("view engine", "ejs");         // EJS set करा
-//app.set("views", __dirname + "/views"); // Views फोल्डरचं path
+app.set("view engine", "ejs");    //set ejs    
+
 
 module.exports = app;
--- src/routes/homeRouts.js
+++ src/routes/homeRouts.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const regCtrl = require("../controller/regCtrl"); 
 
 router.get("/", (req, res) => {
-    res.render("home"); // renders views/home.ejs
+    res.render("home");
+});
+
+router.get("/regCtrl",(req,res)=>
+{
+    res.render("login");
 });
 
 module.exports = router;
