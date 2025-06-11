f3495e48c617637e36b02af2cda46209dac5f49f
Prathmesh <prathmeshgunjal149@gmail.com>
2025-06-11 15:17:12 +0530

    some minior changes done

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ let express=require("express");
 let app=express();
  let conn=require("./config/db.js");
 
-
-
-
+const homeRoutes = require("./routs/homeRoutes");
+app.use("/", homeRoutes);
 module.exports=app;
+
+
