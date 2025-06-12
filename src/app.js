dd38fd851fb3259f217a57a4518ed49e8362b79d
Rutuja <rutujar0407@gmail.com>
2025-06-12 14:22:20 +0530

    changes done

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 let express = require("express");
 let app = express();
 let conn = require("./config/db.js");
-const homeRoutes = require("./routes/homeRouts");
-app.use("/", homeRoutes);
+
 app.set("view engine", "ejs");    //set ejs    
 
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+const homeRoutes = require("./routes/homeRouts");
+app.use("/", homeRoutes);
 module.exports = app;
--- src/routes/homeRouts.js
+++ src/routes/homeRouts.js
@@ -20,36 +20,42 @@ router.get("/register", (req, res) => {
 router.get("/", (req, res) => {
     res.render("home");
 });
+// admin dashboard
+router.get("/", regCtrl.regCtrl);
 
 // Register Page
-router.get("/register", (req, res) => {
+/*router.get("/register", (req, res) => {
     res.render("register"); // views/register.ejs
-});
+});*/
 
 // Handle Login Form Submission
 router.post("/login", (req, res) => {
-    const { username, password } = req.body;
-    // Authentication logic here (dummy for now)
-    if (username === "admin" && password === "1234") {
-        res.send("Login Successful!");
-    } else {
-        res.send("Invalid credentials");
-    }
-});
+  const { username, password } = req.body;
 
-// Register page route
-router.get("/register", (req, res) => {
-    res.render("register");
+      db.query("select *from admin where username=? and password=?");
+      [username,password],(err,result)=>
+      {
+           if(err)
+           {
+               console.log("database error",err); 
+           }
+      }
+  
+
 });
+// Register page route
+/*router.get("/register", (req, res) => {
+    res.render("register");
+});*/
 
 // Handle register form submission
-router.post("/register", (req, res) => {
+/*router.post("/register", (req, res) => {
     const { fullname, email, username, password } = req.body;
 
     // TODO: Add database insert logic here
     console.log("User registered:", fullname, email, username);
 
     res.send("Registration successful! (This is just a placeholder)");
-});
+});*/
 
 module.exports = router;
